f6c99819040f31ef65b4241b476b537d28b464fe
ismail <ikhan.mail1@gmail.com>
2023-02-15 16:00:49 +0500

    new

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_dDweLFdpq', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1mdoiupqe7dkmsptvo57iirqvf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://8mlw2mczdd.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
